2d0c4250733a3ec371cff34a2e348f1450ea7cbf
Linda5-umwali <l.umwali5@alustudent.com>
2025-05-19 03:43:43 -0700

    Added the URL regex

--- script.py
+++ script.py
@@ -9,4 +9,6 @@ text = input("Enter the text you want to extract data from:\n")
 emails = re.findall(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', text)
 print("Emails:", emails)
 
-
+# URL regex
+urls = re.findall(r'https?://[^\s]+', text)
+print("URLs:", urls)
