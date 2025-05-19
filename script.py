72a97683541bd12941594ac7970de90efd629da5
Linda5-umwali <l.umwali5@alustudent.com>
2025-05-19 03:41:24 -0700

    Added Email regex

---
+++ script.py
@@ -0,0 +1,12 @@
+import re
+
+# Sample input text
+text = input("Enter the text you want to extract data from:\n")
+
+# Regex patterns below
+
+# Email regex
+emails = re.findall(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', text)
+print("Emails:", emails)
+
+
